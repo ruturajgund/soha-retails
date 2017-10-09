e1a4c5314cd2ceb2d5e2afc5abbc954a9dcbb579
OSIUS\rgund <Ruthuraj_Devanand_Gund@gap.com>
2017-10-09 09:52:43 +0530

    Added Grid in order tab.

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -9,16 +9,18 @@ import { Order } from '../models/index'
 })
 export class OrdersComponent implements OnInit {
   orders: any[] = [];
+  spinnerFlag: boolean = false;
   constructor(
     private ordersServices: OrdersServices
   ) { }
 
   ngOnInit() {
+    this.spinnerFlag = true;
     this.ordersServices.getAllOrders().subscribe(
       response => {
         var data = response.json();
         this.orders = data.orders;
-        console.log(this.orders);
+        this.spinnerFlag = false;
       })
   }
 }
